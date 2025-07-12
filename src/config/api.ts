4dfc175c35760adeb709f095dcb145ee36eccb1a
ninors <143431164+ninors@users.noreply.github.com>
2025-07-12 19:38:33 +0200

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 
 // Configuration API ChartFox
 // Remplacez 'YOUR_API_KEY_HERE' par votre vraie clé API ChartFox
-export const CHARTFOX_API_KEY = 'YOUR_API_KEY_HERE';
+export const CHARTFOX_API_KEY = ' 557d3c0f-d8a8-4b5c-9727-572a117d9fc5';
 
 // URL de base de l'API ChartFox
 export const CHARTFOX_BASE_URL = 'https://api.chartfox.org/v2';
